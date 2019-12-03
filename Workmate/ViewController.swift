1acd4dd997d10bd0b9a2569290f4b2ddd4f8178c
Bilguun <bilguun132@gmail.com>
2019-12-03 12:32:06 +0800

    Initial Commit

---
+++ Workmate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Workmate
+//
+//  Created by Bilguun Batbold on 3/12/19.
+//  Copyright © 2019 Bilguun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
